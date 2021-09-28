fab5c36ec1220e9b971f8a06012fa76875a9b153
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-09-28 13:44:20 -0700

    added utils and can render to local host from index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
 
+const { myNumber } = require('./utils.js');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`Hello World! ${myNumber}`)
 })
 
 app.listen(port, () => {
---
+++ utils.js
@@ -0,0 +1,5 @@
+const myNumber = 7;
+
+module.exports = {
+    myNumber,
+}
